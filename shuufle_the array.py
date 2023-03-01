3acd0a871df05122a70ccbce063c4807145fedd1
Ashwin Chavan <86465817+Ash7540@users.noreply.github.com>
2023-03-01 20:53:55 +0530

    Delete shuufle_the array.py

--- shuufle_the array.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def shuffle(self, nums: List[int], n: int) -> List[int]:
-        shuffle=[]
-        for j in range(n):
-            shuffle+=[nums[j]]
-            shuffle+=[nums[j+n]]
-        return shuffle
